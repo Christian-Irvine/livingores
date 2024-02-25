5aea3ef9f1da2660210b5ca56656003e01224614
Christian Irvine <christian.irvine04@gmail.com>
2024-02-25 14:18:05 +1300

    Initial Setup of mod

---
+++ src/main/java/me/craftymcfish/livingores/LivingOresClient.java
@@ -0,0 +1,10 @@
+package me.craftymcfish.livingores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LivingOresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
